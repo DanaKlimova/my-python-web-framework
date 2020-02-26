a1d90a784eb12816dd38f42ac54d3081942a40d7
Dana Klimova <dana.klimava@itechart-group.com>
2020-02-26 13:36:02 +0300

    Added middleware

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from wsgiref.simple_server import make_server
 from app import application
+from middleware import Reversware
 
-server = make_server('localhost', 8000, app=application)
+server = make_server('localhost', 8000, app=Reversware(application))
 server.serve_forever()
